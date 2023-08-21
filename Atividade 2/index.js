61968a72a79b36dbf7ce2073ec1038c191e54428
Breno Ferraz Ferreira <81932347+BrenoFerrazFerreira@users.noreply.github.com>
2023-08-21 00:43:35 -0300

    Add files via upload

---
+++ Atividade 2/index.js
@@ -0,0 +1,14 @@
+// Quando o usuário fizer scroll na página,
+// irá disparar a função showProgressBar
+showProgressBar = () => {
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.getElementById("js-barraDeLeitura").style.width = `${scrolled}%`;
+  };
+  window.onscroll = () => {
+    showProgressBar();
+  };
